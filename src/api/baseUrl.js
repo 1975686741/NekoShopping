cd56782091209f299dd0117d22d175b831b0dca9
1975686741 <1975686741@qq.com>
2023-04-26 14:04:57 +0800

    接口

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 let baseURL = "";
 if (process.env.NODE_ENV === "development") {
-  baseURL = "http://shop.bufantec.com";
+  baseURL = "https://www.fastmock.site/mock/1f8ee588ba1482f1400f70afe7a6edee/NekoShopping";
 } else if (process.env.NODE_ENV === "production") {
-  baseURL = "http://shop.bufantec.com";
+  baseURL = "https://www.fastmock.site/mock/1f8ee588ba1482f1400f70afe7a6edee/NekoShopping";
 }
 export default baseURL;
